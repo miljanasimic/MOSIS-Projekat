eaa533c5c53184134a0f28fdaabee0c47bb098c3
miljanasimic <simic.mimi@gmail.com>
2022-06-05 20:11:04 +0200

    login fragment, registration fragment

--- PetFinder/app/src/main/java/elfak/mosis/petfinder/MainActivity.kt
+++ PetFinder/app/src/main/java/elfak/mosis/petfinder/MainActivity.kt
@@ -1,11 +1,32 @@
 package elfak.mosis.petfinder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import elfak.mosis.petfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
---
+++ PetFinder/app/src/main/java/elfak/mosis/petfinder/WelcomeFragment.kt
@@ -0,0 +1,41 @@
+package elfak.mosis.petfinder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import elfak.mosis.petfinder.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_WelcomeFragment_to_LoginFragment)
+        }
+
+        binding.buttonSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_WelcomeFragment_to_RegistrationFragment)
+        }
+    }
+
+}
