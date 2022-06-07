45a754ea4e5c467691e0d5378fa97a107e4dba2b
miljanasimic <simic.mimi@gmail.com>
2022-06-07 02:26:54 +0200

    firebase dodat, ui za registraciju, deo za kameru

--- PetFinder/app/src/main/java/elfak/mosis/petfinder/RegistrationFragment.kt
+++ PetFinder/app/src/main/java/elfak/mosis/petfinder/RegistrationFragment.kt
@@ -1,16 +1,21 @@
 package elfak.mosis.petfinder
 
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import elfak.mosis.petfinder.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
     private var _binding: FragmentRegistrationBinding? = null
-
+    val REQUEST_IMAGE_CAPTURE = 1
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -28,7 +33,24 @@ class RegistrationFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.textLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_RegistrationFragment_to_LoginFragment)
+        }
+        binding.camera.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+            }
 
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageView.setImageBitmap(imageBitmap)
+        }
     }
 
 }
