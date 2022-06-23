923cf57df5cb23cbfa628ad40586d2eef91aba57
miljanasimic <simic.mimi@gmail.com>
2022-06-23 02:06:40 +0200

    trazenje uredjaja

--- PetFinder/app/src/main/java/elfak/mosis/petfinder/ui/friends/ConnectFriendsFragment.kt
+++ PetFinder/app/src/main/java/elfak/mosis/petfinder/ui/friends/ConnectFriendsFragment.kt
@@ -1,21 +1,37 @@
 package elfak.mosis.petfinder.ui.friends
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
-import android.bluetooth.BluetoothAdapter
+import android.app.AlertDialog
+import android.bluetooth.*
+import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import elfak.mosis.petfinder.databinding.FragmentConnectFriendsBinding
+import java.io.IOException
+import java.util.*
 
 class ConnectFriendsFragment : Fragment() {
     private var _binding: FragmentConnectFriendsBinding? = null
     private val binding get() = _binding!!
-    private var  bAdapter: BluetoothAdapter? =null
+
+    private lateinit var bluetoothAdapter: BluetoothAdapter
     private val REQUEST_CODE_ENABLE_BT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -28,37 +44,148 @@ class ConnectFriendsFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (bAdapter!!.isEnabled)
-            print("Bl on")
-        else
-            print("Bl off")
-        binding.buttonTurnOnBluetooth.setOnClickListener {
-            if (bAdapter!!.isEnabled)
-                Toast.makeText(view.context, "vec ukljucen", Toast.LENGTH_SHORT).show()
-            else{
-                var intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(intent, REQUEST_CODE_ENABLE_BT)
-            }
-        }
+        val bluetoothManager: BluetoothManager? = ContextCompat.getSystemService(requireContext(), BluetoothManager::class.java)
+        bluetoothAdapter = bluetoothManager!!.adapter
+        if (!bluetoothAdapter.isEnabled) enableBluetooth(view.context)
+        checkLocationPermission()
         binding.buttonDiscover.setOnClickListener{
+
+            val requestCode = 1;
+            val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+            }
+            startActivityForResult(discoverableIntent, requestCode)
+            val isSuccess=bluetoothAdapter.startDiscovery()
+            val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+            activity?.registerReceiver(receiver, filter)
+
         }
+    }
+
+
+    private fun enableBluetooth(context: Context) {
+        Toast.makeText(context, "In order to connect to other users, you have to enable Bluetooth.", Toast.LENGTH_SHORT).show()
+        val enableBlIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(enableBlIntent, REQUEST_CODE_ENABLE_BT)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_ENABLE_BT) {
             if( resultCode == Activity.RESULT_OK){
-                if(bAdapter!!.isEnabled)
-                    Toast.makeText(view?.context, "Dozvoljen", Toast.LENGTH_SHORT).show()
+                if(bluetoothAdapter.isEnabled){
+                    Toast.makeText(view?.context, "Bluetooth enabled.", Toast.LENGTH_SHORT).show()
+                }
                 else
-                    Toast.makeText(view?.context, "Nije Dozvoljen", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(view?.context, "Bluetooth is not enabled. Please try again", Toast.LENGTH_SHORT).show()
             } else if (resultCode == Activity.RESULT_CANCELED)
-                Toast.makeText(view?.context, "cancelled", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(view?.context, "Cancelled", Toast.LENGTH_SHORT).show()
         }
     }
 
+    private val receiver = object : BroadcastReceiver() {
+
+        @SuppressLint("MissingPermission")
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String? = intent.action
+            Toast.makeText(requireContext(), intent.toString(), Toast.LENGTH_LONG).show()
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onDestroy() {
+        activity?.unregisterReceiver(receiver)
+        bluetoothAdapter.cancelDiscovery()
+        super.onDestroy()
+
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            MY_PERMISSIONS_REQUEST_LOCATION
+        )
+    }
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_LOCATION = 99
+        private const val MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION = 66
+    }
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton(
+                        "OK"
+                    ) { _, _ ->
+                        //Prompt the user once explanation has been shown
+                        requestLocationPermission()
+                    }
+                    .create()
+                    .show()
+            } else {
+                // No explanation needed, we can request the permission.
+                requestLocationPermission()
+            }
+        } else {
+            checkBackgroundLocation()
+        }
+    }
+
+    private fun checkBackgroundLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestBackgroundLocationPermission()
+        }
+    }
+
+    private fun requestBackgroundLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ),
+                MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSIONS_REQUEST_LOCATION
+            )
+        }
+    }
 }
